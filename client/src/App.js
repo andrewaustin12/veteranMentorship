77dcca6ce83da46762a946c4dbb53fe265384528
jmdayoub <jdayoub@gmail.com>
2022-11-19 12:09:53 -0800

    Linked Sign-in to Register via Link

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,21 @@ import Resources from "./pages/Resources";
 import Dashboard from "./pages/Dashboard";
 import Navbar from "./components/Navbar";
 import Mentorship from "./pages/Mentorship";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    <> 
+    <>
       <div>
-      <Navbar/>
+        <Navbar />
         <Router>
           <Routes>
-              <Route exact path="/" element={<LandingPage />} />
-              <Route exact path="/dashboard" element={<Dashboard />} />
-              <Route exact path="/mentorship" element={<Mentorship />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/resources" element={<Resources />} />
+            <Route exact path="/" element={<LandingPage />} />
+            <Route exact path="/signin" element={<SignIn />} />
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/mentorship" element={<Mentorship />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/resources" element={<Resources />} />
           </Routes>
         </Router>
       </div>
---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LoginForm from "../components/forms/LoginForm";
+
+const SignIn = () => {
+  return (
+    <>
+      <div className="d-flex">
+        <div className="justify-content-center">
+          <LoginForm />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
