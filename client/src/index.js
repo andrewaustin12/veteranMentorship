427e71b9fd6216e029cd51c1f744417c83c2cd78
jmdayoub <jdayoub@gmail.com>
2022-11-19 13:03:03 -0800

    Fixed merge conflict on index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
->>>>>>> main
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
