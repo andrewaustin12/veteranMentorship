edc3646eee73acf7ac31a3f43d681ae8051802dc
jmdayoub <jdayoub@gmail.com>
2022-11-20 08:30:51 -0800

    Minor css changes and added shell of chat feature

--- client/src/components/LandingPageComponent.js
+++ client/src/components/LandingPageComponent.js
@@ -1,50 +1,57 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
 
 const tiers = [
   {
-    title: 'Mentor',
-    subheader: 'Sign up to become a mentor',
+    title: "Mentor",
+    subheader: "Sign up to become a mentor",
     description: [
-      'Find a mentee',
-      'Offer support and guidance',
-      'Give back to the community',
+      "Find a mentee",
+      "Offer support and guidance",
+      "Give back to the community",
     ],
-    buttonText: 'Sign up',
-    buttonVariant: 'outlined',
-    buttonRoute: '/signup',
+    buttonText: "Sign up",
+    buttonVariant: "outlined",
+    buttonRoute: "/signup",
   },
   {
-    title: 'Mentee',
-    subheader: 'Join to receive mentorship',
+    title: "Mentee",
+    subheader: "Join to receive mentorship",
     description: [
-      'Find a mentor',
-      'Find a community',
-      'Get guidance and support',
+      "Find a mentor",
+      "Find a community",
+      "Get guidance and support",
     ],
-    buttonText: 'Get started',
-    buttonVariant: 'contained',
-    buttonRoute: '/login',
-  }
+    buttonText: "Get started",
+    buttonVariant: "contained",
+    buttonRoute: "/login",
+  },
 ];
 
 function LandingPageContent() {
   return (
     <React.Fragment>
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <GlobalStyles
+        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+      />
       <CssBaseline />
-      <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+      <Container
+        disableGutters
+        maxWidth="sm"
+        component="main"
+        sx={{ pt: 8, pb: 6 }}
+      >
         <Typography
           component="h1"
           variant="h2"
@@ -54,9 +61,15 @@ function LandingPageContent() {
         >
           Welcome to Veteran Mentorship
         </Typography>
-        <Typography variant="h5" align="center" color="text.secondary" component="p">
-          Veteran Mentorship is a program that strives to help better assist veterans adjust to civilian life. We strive to provide veterans
-          with mentors to help guide them as well as useful resources.
+        <Typography
+          variant="h5"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Veteran Mentorship is a program that strives to help better assist
+          veterans adjust to civilian life. We strive to provide veterans with
+          mentors to help guide them as well as useful resources.
         </Typography>
       </Container>
       {/* End hero unit */}
@@ -68,20 +81,20 @@ function LandingPageContent() {
               item
               key={tier.title}
               xs={12}
-              sm={tier.title === 'Enterprise' ? 12 : 6}
+              sm={tier.title === "Enterprise" ? 12 : 6}
               md={4}
             >
               <Card>
                 <CardHeader
                   title={tier.title}
                   subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
+                  titleTypographyProps={{ align: "center" }}
                   subheaderTypographyProps={{
-                    align: 'center',
+                    align: "center",
                   }}
                   sx={{
                     backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
+                      theme.palette.mode === "light"
                         ? theme.palette.grey[200]
                         : theme.palette.grey[700],
                   }}
@@ -89,13 +102,12 @@ function LandingPageContent() {
                 <CardContent>
                   <Box
                     sx={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'baseline',
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "baseline",
                       mb: 2,
                     }}
-                  >
-                  </Box>
+                  ></Box>
                   <ul>
                     {tier.description.map((line) => (
                       <Typography
@@ -110,11 +122,9 @@ function LandingPageContent() {
                   </ul>
                 </CardContent>
                 <CardActions>
-                  {/* Redirect to a route buttonURL: /register, login/, etc. */}
                   <Link href="/register" underline="none" sx={{ flexGrow: 1 }}>
                     <Button fullWidth variant={tier.buttonVariant}>
                       {tier.buttonText}
-                      {/* Add url to /register */}
                     </Button>
                   </Link>
                 </CardActions>
